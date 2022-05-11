e862f75ecd55cf721a296e216689c02acbb22c1a
Matias <matiascanalicchio@gmail.com>
2022-05-11 15:17:15 -0300

    elketchap

--- scrpit.js
+++ scrpit.js
@@ -1,3 +1,21 @@
-function enviar(){
-    alert("Se Envio Correctamente");
+let valor1 = Math.floor(Math.random()*10);
+
+let valor2 = Math.floor(Math.random()*10);
+
+let captcha = document.getElementById("captcha");
+
+captcha.innerHTML = valor1 + "+" + valor2;
+
+let valor3 = document.getElementById("respuesta");
+
+let final = document.getElementById("resultado");
+
+function enviar(event){
+    if ((valor1 + valor2) == valor3.value) {
+        final.innerHTML = "El captcha es correcto";
+        event.preventDefault();
+    } else {
+        final.innerHTML = "El captcha es incorrecto";
+        event.preventDefault();
+    }
 }
