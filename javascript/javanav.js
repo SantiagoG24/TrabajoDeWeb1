91e17d96a3e9dec7e98a943359da1c2766d702d2
Santiago Gonzalez <SantiagoGonzalez_2002@hotmail.com>
2022-06-09 16:01:06 -0300

    agregue el nav desplegable

---
+++ javascript/javanav.js
@@ -0,0 +1,15 @@
+let navegador=document.querySelector(".Navigation");
+let menu =document.querySelector("#BotonMenu");
+menu.addEventListener('click', function (){
+    navegador.classList.toggle("visible");
+});
+
+
+
+
+
+
+
+
+
+
