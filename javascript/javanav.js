20abe73b354a3042d415c999450a1e29c665d214
MatiasCanalicchio <93134450+MatiasCanalicchio@users.noreply.github.com>
2022-06-18 11:16:36 -0300

    arranque del nav nuevo

--- javascript/javanav.js
+++ javascript/javanav.js
@@ -5,7 +5,29 @@ function menu_desplegable(event){
     navegador.classList.toggle("visible");
 };
 
+async function cargar(){
+    let container = document.querySelector("#escritura");
+    container.innerHTML = "<h1>Loading...</h1>";
+    try{
+        let response = await fetch(url);
+        if (response.ok){
+            let t = await response.text();
+            container.innerhtml = t;
+        }else{
+            container.innerhtml = "<h1>Error - Failed URL!</h1>";
+        }
+    }catch(error){
+        container.innerHTML = "<h1>Connection error</h1>";
+    }
+}
 
+let btnH = document.querySelector("#btn-home");
+
+btnH.addEventListener('click',function (){
+    cargar("home.html");
+});
+
+cargar("home.html");
 
 
 
