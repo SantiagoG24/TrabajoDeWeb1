182bed87f929d8784ce05524ecf72b66a2adf42b
Santiago Gonzalez <SantiagoGonzalez_2002@hotmail.com>
2022-07-05 20:39:17 -0300

    Hice todo el rest

--- javascript/formscript.js
+++ javascript/formscript.js
@@ -1,71 +1,59 @@
-// formulario
+const url = 'https://62b60cf56999cce2e8fe5e2a.mockapi.io/api/v1/Horarios';
+let valor1 = Math.floor(Math.random() * 10);//genero los numeros random
 
-let valor1 = Math.floor(Math.random()*10);
+let valor2 = Math.floor(Math.random() * 10);
 
-let valor2 = Math.floor(Math.random()*10);
+function documentoCargado() {
+    let captcha = document.getElementById("captcha");
+    captcha.innerHTML = valor1 + "+" + valor2;
+    let betnx3 = document.querySelector("#buttonx3");
+    let btn = document.querySelector("#buttom");
+    btn.addEventListener('click', enviar);
+    betnx3.addEventListener('click', agregarx3);
+    let msg =document.querySelector("#msg");
 
-let captcha = document.getElementById("captcha");
 
-captcha.innerHTML = valor1 + "+" + valor2;
+}
 
-let final = document.getElementById("resultado");
-
-let betnx3 = document.querySelector("#buttonx3");
-
-let refreshflase = document.getElementById("respuesta");
-
-let btn = document.querySelector("#buttom");
-
-btn.addEventListener('click', enviar);
-
-function enviar(event){
+function enviar(event) {
     event.preventDefault();
+    let final = document.getElementById("resultado");
     let nombre = document.querySelector('#nombre').value;
     let mail = document.querySelector('#mail').value;
     let hora = document.querySelector('#horario').value;
-    if ((nombre != "") && (mail != "") && (hora != "")) { 
         if (validarcaptcha()) {
             final.innerHTML = "El captcha es correcto";
-            agregarcliente(nombre,mail,hora);
+            agregarcliente(nombre, mail, hora);
+        } else {
+            msg=final.innerHTML = "El captcha es incorrecto";
+        }
+}
+function agregarx3(event) {
+    event.preventDefault();
+    let final = document.getElementById("resultado");
+    let nombre = document.querySelector('#nombre').value;
+    let mail = document.querySelector('#mail').value;
+    let hora = document.querySelector('#horario').value;
+        if (validarcaptcha()) {
+            final.innerHTML = "El captcha es correcto";
+            for (let index = 0; index < 3; index++) {
+                agregarcliente(nombre, mail, hora++);
+            }
         } else {
             final.innerHTML = "El captcha es incorrecto";
         }
-    } else{
-        final.innerHTML = "Dejo algun campo sin rellenar";
-    }
-}
-    
-betnx3.addEventListener('click',agregarx3);
-
-function agregarx3(event){
-    event.preventDefault();
-    let nombre = document.querySelector('#nombre').value;
-    let mail = document.querySelector('#mail').value;
-    let hora = document.querySelector('#horario').value;
-    if ((nombre != "") && (mail != "") && (hora != "")) {    
-        if (validarcaptcha()){
-            final.innerHTML = "El captcha es correcto";
-            for (let index = 0; index < 3; index++) {
-                agregarcliente(nombre,mail,hora++);
-            }
-        }else{
-            final.innerHTML = "El captcha es incorrecto";
-        }
-    } else{
-        final.innerHTML = "Dejo algun campo sin rellenar";
-    }
 }
 
-function validarcaptcha(){
+function validarcaptcha() {
     let valor3 = document.getElementById("respuesta");
-    if ((valor1 + valor2) == valor3.value){
-        valor1 = Math.floor(Math.random()*10);
-        valor2 = Math.floor(Math.random()*10);
+    if ((valor1 + valor2) == valor3.value) {
+        valor1 = Math.floor(Math.random() * 10);
+        valor2 = Math.floor(Math.random() * 10);
         captcha.innerHTML = valor1 + "+" + valor2;
         return true;
     }
-    valor1 = Math.floor(Math.random()*10);
-    valor2 = Math.floor(Math.random()*10);
+    valor1 = Math.floor(Math.random() * 10);
+    valor2 = Math.floor(Math.random() * 10);
     captcha.innerHTML = valor1 + "+" + valor2;
     valor3.value = "";
     return false;
@@ -78,23 +66,101 @@ function validarcaptcha(){
 let cliente = [
 ]
 
-function agregarcliente(nombre,mail,hora){
-    while(hora>24){
-        hora-=24;
-    }
+async function agregarcliente(nombre, mail, hora) {
     let clientenuevo = {
-        name: nombre,
-        mail:mail,
-        hour:hora
+        "Nombre": nombre,
+        "Mail": mail,
+        "Hora": hora.value
     }
-    cliente.push(clientenuevo);
-    imprimir();
+    console.log(clientenuevo);
+    try {
+        let res= await fetch(url,{
+            "method":"POST",
+            "headers":{"Content-type":"application/json"},
+            "body":JSON.stringify(clientenuevo)
+        });
+        if(res.status===201){
+            msg.innerHTML="Creado!";
+        }   
+    } catch (error) {
+        console.log(error)
+    }
+    // cliente.push(clientenuevo);
+    // imprimir();
 }
 
-function imprimir(){
+function imprimir() {
     let inscripto = document.querySelector('#lista');
     inscripto.innerHTML = '';
     for (const orden of cliente) {
         inscripto.innerHTML += "<tr>" + "<td>" + orden.name + "</td>" + "<td>" + orden.mail + "</td>" + "<td>" + orden.hour + "</td>" + "</tr>";
+    }
+}
+async function actualizarApi() {
+    try {
+        let res = await fetch(url);
+        let json = await res.json();
+        let inscripto = document.querySelector('#lista');
+        inscripto.innerHTML = '';
+        for (const Horarios of json) {
+            let nombre = Horarios.Nombre;
+            let mail = Horarios.Mail;
+            let Hora = Horarios.Hora;
+            let Id = Horarios.id;
+            inscripto.innerHTML += "<tr >" + "<td class='fila_tabla'>" + Horarios.Nombre + "</td>" + "<td class='fila_tabla'>" + Horarios.Mail + "</td>" + "<td class='fila_tabla'>" + Horarios.Hora + "</td>" + "<td class='fila_tabla'><button class='borrar'style='padding: 5px' id=" + Id + ">"+"<i class='bi-trash'></i>"+"</button></td>" + "<td class='fila_tabla'><button class='editar' id=" + Id +">"+"<i class='bi bi-pencil'></i>" +"</button></td>" + "</tr>";
+        }
+        let btn_borrar = document.querySelectorAll(".borrar");
+        for (let i=0 ;i< btn_borrar.length;i++){
+            btn_borrar[i].addEventListener("click",function(e){
+                borrar(this.id);
+            });
+        }
+        let btn_editar = document.querySelectorAll(".editar");
+        for (let i=0 ; i< btn_editar.length ;i++){
+            btn_editar[i].addEventListener("click",function(e){
+                editar(this.id);
+            });
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+async function borrar(id) {
+    let url_id = `${url}/${id}`;
+    try {
+        let res = await fetch(url_id, {
+            "method": "DELETE"
+        });
+        if (res.status === 200) {
+            document.querySelector("#msg").innerHTML = "Se ha eliminado su reservacion "
+        }
+    actualizarApi();
+    } catch (error) {
+        console.log(error);
+    }
+}
+async function editar(id) {
+    let nombre = document.querySelector('#nombre').value;
+    let mail = document.querySelector('#mail').value;
+    let hora = document.querySelector('#horario').value;
+    let url_id = `${url}/${id}`;
+    let editar = {
+        "Nombre": nombre,
+        "Mail": mail,
+        "Hora": hora
+    }
+    try {
+        let res = await fetch(url_id, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(editar)
+        });
+        if (res.status === 200) {
+            document.querySelector("#msg").innerHTML = "Su cambio se agrego"
+        }
+    } catch (error) {
+        console.log(error);
     }
 }
--- javascript/javanav.js
+++ javascript/javanav.js
@@ -1,44 +1,40 @@
-let navegador=document.querySelector(".Navigation");
-let menu =document.querySelector("#BotonMenu");
-menu.addEventListener('click',menu_desplegable);
-function menu_desplegable(event){
+let navegador = document.querySelector(".Navigation");
+let menu = document.querySelector("#BotonMenu");
+menu.addEventListener('click', menu_desplegable);
+function menu_desplegable(event) {
     navegador.classList.toggle("visible");
 };
 
-async function cargar(url){
+async function cargar(url) {
     let container = document.querySelector("#escritura");
     container.innerHTML = "<h1>Loading...</h1>";
-    try{
+    try {
         let response = await fetch(url);
-        if (response.ok){
+        if (response.ok) {
             let t = await response.text();
             container.innerHTML = t;
-        }else{
+            if (url == "computadoras.html") {
+                documentoCargado();
+                actualizarApi();
+            }
+        } else {
             container.innerHTML = "<h1>Error - Failed URL!</h1>";
         }
-    }catch(error){
+    } catch (error) {
         container.innerHTML = "<h1>Connection error</h1>";
     }
 }
 
-document.querySelector("#btn-home").addEventListener('click',function (){
+document.querySelector("#btn-home").addEventListener('click', function () {
     cargar("home.html");
 });
 
-document.querySelector("#btn-comida").addEventListener('click',function (){
+document.querySelector("#btn-comida").addEventListener('click', function () {
     cargar("comida.html");
 });
 
-document.querySelector("#btn-computadoras").addEventListener('click',function (){
+document.querySelector("#btn-computadoras").addEventListener('click', function () {
     cargar("computadoras.html");
 });
 
 cargar("home.html");
-
-
-
-
-
-
-
-
